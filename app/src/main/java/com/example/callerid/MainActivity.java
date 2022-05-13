23bbf8e7c0d1f607078e6101ab5debe61a9f46aa
charbelmattarr <74071943+charbelmattarr@users.noreply.github.com>
2022-05-13 15:26:11 +0300

    Phone Number Detection added

--- app/src/main/java/com/example/callerid/MainActivity.java
+++ app/src/main/java/com/example/callerid/MainActivity.java
@@ -1,14 +1,435 @@
 package com.example.callerid;
 
+import static javax.ws.rs.HttpMethod.GET;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.NameValuePair;
+import com.google.gson.JsonObject;
+import com.microsoft.graph.authentication.IAuthenticationProvider; //Imports the Graph sdk Auth interface
+import com.microsoft.graph.concurrency.ICallback;
+import com.microsoft.graph.core.ClientException;
+import com.microsoft.graph.http.IHttpRequest;
+import com.microsoft.graph.models.extensions.*;
+import com.microsoft.graph.options.Option;
+import com.microsoft.graph.options.QueryOption;
+import com.microsoft.graph.requests.extensions.ContactCollectionPage;
+import com.microsoft.graph.requests.extensions.GraphServiceClient;
+import com.microsoft.identity.client.AuthenticationCallback; // Imports MSAL auth methods
+import com.microsoft.identity.client.*;
+import com.microsoft.identity.client.exception.*;
+
+import org.json.JSONObject;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static String[] SCOPES = {"Files.Read"};
+    /* Azure AD v2 Configs */
+    final static String AUTHORITY = "https://login.microsoftonline.com/common";
+    private ISingleAccountPublicClientApplication mSingleAccountApp;
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    /* UI & Debugging Variables */
+    Button signInButton;
+    Button signOutButton;
+    Button callGraphApiInteractiveButton;
+    Button callGraphApiSilentButton;
+
+ //   Button callGraphApiInteractiveButton2;
+ //   Button callGraphApiSilentButton2;
+
+    TextView logTextView;
+    TextView currentUserTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE)
+                != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_PHONE_STATE},1);
+        }
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CALL_LOG)
+                != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_CALL_LOG},1);
+        }
+        this.runOnUiThread(new Runnable() {
+
+            @Override
+            public void run() {
+
+                // Stuff that updates the UI
+                initializeUI();
+            }
+        });
+
+        //initializeUI();
+
+        PublicClientApplication.createSingleAccountPublicClientApplication(getApplicationContext(),
+                R.raw.auth_config_single_account, new IPublicClientApplication.ISingleAccountApplicationCreatedListener() {
+                    @Override
+                    public void onCreated(ISingleAccountPublicClientApplication application) {
+                        mSingleAccountApp = application;
+                        loadAccount();
+                    }
+                    @Override
+                    public void onError(MsalException exception) {
+                        displayError(exception);
+                    }
+                });
     }
+
+    //When app comes to the foreground, load existing account to determine if user is signed in
+    private void loadAccount() {
+        if (mSingleAccountApp == null) {
+            return;
+        }
+
+        mSingleAccountApp.getCurrentAccountAsync(new ISingleAccountPublicClientApplication.CurrentAccountCallback() {
+            @Override
+            public void onAccountLoaded(@Nullable IAccount activeAccount) {
+                // You can use the account data to update your UI or your app database.
+                updateUI(activeAccount);
+            }
+
+            @Override
+            public void onAccountChanged(@Nullable IAccount priorAccount, @Nullable IAccount currentAccount) {
+                if (currentAccount == null) {
+                    // Perform a cleanup task as the signed-in account changed.
+                    performOperationOnSignOut();
+                }
+            }
+
+            @Override
+            public void onError(@NonNull MsalException exception) {
+                displayError(exception);
+            }
+        });
+
+
+    }
+
+
+
+    private void initializeUI(){
+        signInButton = findViewById(R.id.signIn);
+        callGraphApiSilentButton = findViewById(R.id.callGraphSilent);
+        callGraphApiInteractiveButton = findViewById(R.id.callGraphInteractive);
+
+//        callGraphApiSilentButton2 = findViewById(R.id.callGraphSilent2);
+//        callGraphApiInteractiveButton2 = findViewById(R.id.callGraphInteractive2);
+
+        signOutButton = findViewById(R.id.clearCache);
+        logTextView = findViewById(R.id.txt_log);
+        currentUserTextView = findViewById(R.id.current_user);
+
+        //Sign in user
+        signInButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                if (mSingleAccountApp == null) {
+                    System.out.println("in if mSingleAccountApp");
+                    return;
+                }
+                System.out.println("in nottttt if mSingleAccountApp");
+                mSingleAccountApp.signIn(MainActivity.this, null, SCOPES, getAuthInteractiveCallback());
+            }
+        });
+
+        //Sign out user
+        signOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mSingleAccountApp == null){
+                    return;
+                }
+                mSingleAccountApp.signOut(new ISingleAccountPublicClientApplication.SignOutCallback() {
+                    @Override
+                    public void onSignOut() {
+                        updateUI(null);
+                        performOperationOnSignOut();
+                    }
+                    @Override
+                    public void onError(@NonNull MsalException exception){
+                        displayError(exception);
+                    }
+                });
+            }
+        });
+
+        //Interactive
+        callGraphApiInteractiveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mSingleAccountApp == null) {
+                    return;
+                }
+                mSingleAccountApp.acquireToken(MainActivity.this, SCOPES, getAuthInteractiveCallback());
+            }
+        });
+
+        //Silent
+        callGraphApiSilentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mSingleAccountApp == null){
+                    return;
+                }
+                mSingleAccountApp.acquireTokenSilentAsync(SCOPES, AUTHORITY, getAuthSilentCallback());
+            }
+        });
+
+//        //Interactive2
+//        callGraphApiInteractiveButton2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (mSingleAccountApp == null) {
+//                    return;
+//                }
+//                mSingleAccountApp.acquireToken(MainActivity.this, SCOPES, getAuthInteractiveCallback2());
+//            }
+//        });
+//
+//        //Silent2
+//        callGraphApiSilentButton2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (mSingleAccountApp == null){
+//                    return;
+//                }
+//                mSingleAccountApp.acquireTokenSilentAsync(SCOPES, AUTHORITY, getAuthSilentCallback2());
+//            }
+//        });
+
+    }
+
+    private AuthenticationCallback getAuthInteractiveCallback() {
+        return new AuthenticationCallback() {
+            @Override
+            public void onSuccess(IAuthenticationResult authenticationResult) {
+                /* Successfully got a token, use it to call a protected resource - MSGraph */
+                Log.d(TAG, "Successfully authenticated");
+                /* Update UI */
+                updateUI(authenticationResult.getAccount());
+                /* call graph */
+               callGraphAPI(authenticationResult);
+            }
+
+            @Override
+            public void onError(MsalException exception) {
+                /* Failed to acquireToken */
+                Log.d(TAG, "Authentication failed: " + exception.toString());
+                displayError(exception);
+            }
+            @Override
+            public void onCancel() {
+                /* User canceled the authentication */
+                Log.d(TAG, "User cancelled login.");
+            }
+        };
+    }
+
+    private SilentAuthenticationCallback getAuthSilentCallback() {
+        return new SilentAuthenticationCallback() {
+            @Override
+            public void onSuccess(IAuthenticationResult authenticationResult) {
+                Log.d(TAG, "Successfully authenticated");
+                callGraphAPI(authenticationResult);
+            }
+            @Override
+            public void onError(MsalException exception) {
+                Log.d(TAG, "Authentication failed: " + exception.toString());
+                displayError(exception);
+            }
+        };
+    }
+
+    private void callGraphAPI(IAuthenticationResult authenticationResult) {
+
+        final String accessToken = authenticationResult.getAccessToken();
+
+        IGraphServiceClient graphClient =
+                GraphServiceClient
+                        .builder()
+                        .authenticationProvider(new IAuthenticationProvider() {
+                            @Override
+                            public void authenticateRequest(IHttpRequest request) {
+                                Log.d(TAG, "Authenticating request," + request.getRequestUrl());
+                                request.addHeader("Authorization", "Bearer " + accessToken);
+                            }
+                        })
+                        .buildClient();
+        graphClient
+                .me()
+                .drive()
+                .buildRequest()
+                .get(new ICallback<Drive>() {
+                    @Override
+                    public void success(final Drive drive) {
+                        Log.d(TAG, "Found Drive " + drive.id);
+                       displayGraphResult(drive.getRawObject());
+                    }
+
+                    @Override
+                    public void failure(ClientException ex) {
+                        displayError(ex);
+                    }
+                });
+    }
+
+    private void updateUI(@Nullable final IAccount account) {
+        if (account != null) {
+            signInButton.setEnabled(false);
+            signOutButton.setEnabled(true);
+            callGraphApiInteractiveButton.setEnabled(true);
+            callGraphApiSilentButton.setEnabled(true);
+ //           callGraphApiInteractiveButton2.setEnabled(true);
+ //           callGraphApiSilentButton2.setEnabled(true);
+            currentUserTextView.setText(account.getUsername());
+        } else {
+            signInButton.setEnabled(true);
+            signOutButton.setEnabled(false);
+            callGraphApiInteractiveButton.setEnabled(false);
+            callGraphApiSilentButton.setEnabled(false);
+    //        callGraphApiInteractiveButton2.setEnabled(false);
+      //      callGraphApiSilentButton2.setEnabled(false);
+            currentUserTextView.setText("");
+            logTextView.setText("");
+        }
+    }
+
+    private void displayError(@NonNull final Exception exception) {
+        logTextView.setText(exception.toString());
+    }
+
+    private void displayGraphResult(@NonNull final JsonObject graphResponse) {
+       this.runOnUiThread(new Runnable() {
+
+           @Override
+           public void run() {
+
+               logTextView.setText(graphResponse.toString());
+
+           }
+       });
+    }
+
+    private void performOperationOnSignOut() {
+        final String signOutText = "Signed Out.";
+        currentUserTextView.setText("");
+        Toast.makeText(getApplicationContext(), signOutText, Toast.LENGTH_SHORT)
+                .show();
+    }
+
+
+
+
+
+
+//    private AuthenticationCallback getAuthInteractiveCallback2() {
+//        return new AuthenticationCallback() {
+//            @Override
+//            public void onSuccess(IAuthenticationResult authenticationResult) {
+//                /* Successfully got a token, use it to call a protected resource - MSGraph */
+//                Log.d(TAG, "Successfully authenticated");
+//                /* Update UI */
+//                updateUI(authenticationResult.getAccount());
+//                /* call graph */
+//                callGraphAPI2(authenticationResult);
+//            }
+//
+//            @Override
+//            public void onError(MsalException exception) {
+//                /* Failed to acquireToken */
+//                Log.d(TAG, "Authentication failed: " + exception.toString());
+//                displayError(exception);
+//            }
+//            @Override
+//            public void onCancel() {
+//                /* User canceled the authentication */
+//                Log.d(TAG, "User cancelled login.");
+//            }
+//        };
+//    }
+//
+//    private SilentAuthenticationCallback getAuthSilentCallback2() {
+//        return new SilentAuthenticationCallback() {
+//            @Override
+//            public void onSuccess(IAuthenticationResult authenticationResult) {
+//                Log.d(TAG, "Successfully authenticated");
+//                callGraphAPI2(authenticationResult);
+//            }
+//            @Override
+//            public void onError(MsalException exception) {
+//                Log.d(TAG, "Authentication failed: " + exception.toString());
+//                displayError(exception);
+//            }
+//        };
+//    }
+//
+//    private void callGraphAPI2(IAuthenticationResult authenticationResult) {
+//
+//        final String accessToken2 = authenticationResult.getAccessToken();
+//
+//        IGraphServiceClient graphClient =
+//                GraphServiceClient
+//                        .builder()
+//                        .authenticationProvider(new IAuthenticationProvider() {
+//                            @Override
+//                            public void authenticateRequest(IHttpRequest request) {
+//                                Log.d(TAG, "Authenticating request," + request.getRequestUrl());
+//                                request.addHeader("Authorization", "Bearer " + accessToken2);
+//                            }
+//                        })
+//                        .buildClient();
+//
+//        User user = graphClient.me()
+//                .buildRequest()
+//                .get();
+//        Log.d(TAG, "contactssss," + user);
+//
+//
+
+
+//        graphClient
+//                .me()
+//                .drive()
+//                .buildRequest()
+//                .get(new ICallback<Drive>() {
+//                    @Override
+//                    public void success(final Drive drive) {
+//                        Log.d(TAG, "Found Drive " + drive.id);
+//                        displayGraphResult(drive.getRawObject());
+//                    }
+//
+//                    @Override
+//                    public void failure(ClientException ex) {
+//                        displayError(ex);
+//                    }
+//                });
+   // }
+
+
 }
+
